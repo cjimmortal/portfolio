3b6d51a2ee896ef6e22dfdda710d2b8eb8e49a95
Cj Carpon <114817092+cjimmortal@users.noreply.github.com>
2023-05-02 01:02:04 +0800

    .
    .

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
 function seeProjects(){
     document.getElementById('viewProjects').style.display ="block";
+    document.getElementById('one').style.display ="block";
+
+    document.getElementById('fms').style.display ="none";
+    document.getElementById('atcps').style.display ="none";
+    document.getElementById('bmis').style.display ="none";
+    document.getElementById('spotify').style.display ="none";
+    document.getElementById('starbucks').style.display ="none";
+    document.getElementById('bergs').style.display ="none";
+    
+
     document.getElementById('body').style.overflow= "hidden";
     document.getElementById('body').style.paddingRight= "1rem";
 }
@@ -15,12 +25,11 @@ function closeTab(){
     document.getElementById('body').style.overflow= "auto";
 }
 
+
 function openTab(){
     document.getElementById('hidden-tab').style.display="block";
     document.getElementById('body').style.overflow= "hidden";
 }
-
-
 
 
 function myFunction() {
@@ -28,4 +37,102 @@ function myFunction() {
   }
 
 
+
+
+
+
+function fms(){
+    document.getElementById('viewProjects').style.display ="block";
+    document.getElementById('one').style.display ="none";
+
+    document.getElementById('fms').style.display ="block";
+    document.getElementById('atcps').style.display ="none";
+    document.getElementById('bmis').style.display ="none";
+    document.getElementById('spotify').style.display ="none";
+    document.getElementById('starbucks').style.display ="none";
+    document.getElementById('bergs').style.display ="none";
+    
+
+    document.getElementById('body').style.overflow= "hidden";
+    document.getElementById('body').style.paddingRight= "1rem";
+}
+
+function bmis(){
+    document.getElementById('viewProjects').style.display ="block";
+    document.getElementById('one').style.display ="none";
+
+    document.getElementById('fms').style.display ="none";
+    document.getElementById('atcps').style.display ="none";
+    document.getElementById('bmis').style.display ="block";
+    document.getElementById('spotify').style.display ="none";
+    document.getElementById('starbucks').style.display ="none";
+    document.getElementById('bergs').style.display ="none";
+    
+
+    document.getElementById('body').style.overflow= "hidden";
+    document.getElementById('body').style.paddingRight= "1rem";
+}
+
+function atcps(){
+    document.getElementById('viewProjects').style.display ="block";
+    document.getElementById('one').style.display ="none";
+
+    document.getElementById('fms').style.display ="none";
+    document.getElementById('atcps').style.display ="block";
+    document.getElementById('bmis').style.display ="none";
+    document.getElementById('spotify').style.display ="none";
+    document.getElementById('starbucks').style.display ="none";
+    document.getElementById('bergs').style.display ="none";
+    
+
+    document.getElementById('body').style.overflow= "hidden";
+    document.getElementById('body').style.paddingRight= "1rem";
+}
+function bergs(){
+    document.getElementById('viewProjects').style.display ="block";
+    document.getElementById('one').style.display ="none";
+
+    document.getElementById('fms').style.display ="none";
+    document.getElementById('atcps').style.display ="none";
+    document.getElementById('bmis').style.display ="none";
+    document.getElementById('spotify').style.display ="none";
+    document.getElementById('starbucks').style.display ="none";
+    document.getElementById('bergs').style.display ="block";
+    
+
+    document.getElementById('body').style.overflow= "hidden";
+    document.getElementById('body').style.paddingRight= "1rem";
+}
+
+function spotify(){
+    document.getElementById('viewProjects').style.display ="block";
+    document.getElementById('one').style.display ="none";
+
+    document.getElementById('fms').style.display ="none";
+    document.getElementById('atcps').style.display ="none";
+    document.getElementById('bmis').style.display ="none";
+    document.getElementById('spotify').style.display ="block";
+    document.getElementById('starbucks').style.display ="none";
+    document.getElementById('bergs').style.display ="none";
+    
+
+    document.getElementById('body').style.overflow= "hidden";
+    document.getElementById('body').style.paddingRight= "1rem";
+}
+
+function starbucks(){
+    document.getElementById('viewProjects').style.display ="block";
+    document.getElementById('one').style.display ="none";
+
+    document.getElementById('fms').style.display ="none";
+    document.getElementById('atcps').style.display ="none";
+    document.getElementById('bmis').style.display ="none";
+    document.getElementById('spotify').style.display ="none";
+    document.getElementById('starbucks').style.display ="block";
+    document.getElementById('bergs').style.display ="none";
+    
+
+    document.getElementById('body').style.overflow= "hidden";
+    document.getElementById('body').style.paddingRight= "1rem";
+}
   
